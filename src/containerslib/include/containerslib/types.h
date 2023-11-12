1404c1a400e06ef7405f94d4f2c7aa4ac5e07c56
Jheam Storch Ross <jheam.ross@gmail.com>
2023-11-12 18:01:58 -0300

    Add containerslib

---
+++ src/containerslib/include/containerslib/types.h
@@ -0,0 +1,13 @@
+#ifndef _CONTAINERSLIB_TYPES_H_
+#define _CONTAINERSLIB_TYPES_H_
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef void (*free_fn)(void *);
+typedef void *(*cpy_fn)(void *);
+typedef int (*cmp_fn)(const void *, const void *);
+
+typedef unsigned char byte;
+
+#endif
