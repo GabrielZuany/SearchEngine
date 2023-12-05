b715b62300ded5dfc72e466aac771e2de0e5531a
Jheam Storch Ross <jheam.ross@gmail.com>
2023-12-05 00:31:59 -0300

    update index, search, page_indexer

--- src/page_indexerlib/index.c
+++ src/page_indexerlib/index.c
@@ -6,20 +6,26 @@
 struct Index
 {
     StringSt *word_pages_map;
+    StringSet *ref_stop_words;
 };
 
-Index *index_init(StringSt *word_pages_map)
+Index *index_init(StringSt *word_pages_map, StringSet *ref_stop_words)
 {
     Index *index = malloc(sizeof(Index));
     if (index == NULL)
         exception_throw_memory_failure("index_init - Failed to allocate memory for index");
 
     index->word_pages_map = word_pages_map;
+    index->ref_stop_words = ref_stop_words;
 
     return index;
 }
 
-StringSet *index_intersect_pages(Index *index, StringSt *word_pages_subset) {
+StringSet *index_get_ref_stop_words(Index *index) {
+    return index->ref_stop_words;
+}
+
+StringSet *index_intersect_pages(Index *index, StringSet *words) {
     StringSet *pages = stringset_init();
 
     // make the supla clever heap-enabled parallel intersection
