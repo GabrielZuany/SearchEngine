dd55e5ef47c4ca97aa86b99276addbcaea930f01
GabrielZuany <gzuanydev@gmail.com>
2023-12-05 22:09:22 -0300

    rename function name
    page_rank_destroy -> page_rank_finish

--- src/google_page_rankerlib/include/google_page_rankerlib/page_rank.h
+++ src/google_page_rankerlib/include/google_page_rankerlib/page_rank.h
@@ -5,14 +5,13 @@
 #define EPSILON 0.000001 // 10^-6
 
 #include "../../../containerslib/include/containerslib/forward_list.h"
-#include "../../../containerslib/include/containerslib/string_st.h"
-// #include <string_st.h>
+#include "../../../containerslib/include/containerslib/string_set.h"
 
 
 typedef struct PageRank PageRank;
 
 PageRank* page_rank_init(int n_pages);
-void page_rank_destroy(PageRank* self);
+void page_rank_finish(PageRank* self);
 PageRank* page_rank_build_links(PageRank* self, char* graph_path);
 ForwardList* get_out_links_from_page(PageRank* self, char* filename);
 ForwardList* get_in_links_from_page(PageRank* self, char* filename);
